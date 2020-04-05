262c8dc39740763bc65605a2b9c650652e88ddea
vallinayagam39 <60169156+vallinayagam39@users.noreply.github.com>
2020-04-05 23:46:13 +0530

    Add files via upload

---
+++ Internal Storage/app/src/main/java/com/example/internalstorage/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.internalstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class MainActivity extends AppCompatActivity {
+    EditText textmsg;
+    static final int READ_BLOCK_SIZE = 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textmsg=(EditText)findViewById(R.id.editText1);
+    }
+
+    public void WriteBtn(View v) {
+        try {
+            FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
+            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+            outputWriter.write(textmsg.getText().toString());
+            outputWriter.close();
+
+            Toast.makeText(getBaseContext(), "File saved successfully!",Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void ReadBtn(View v) {
+        try {
+            FileInputStream fileIn=openFileInput("mytextfile.txt");
+            InputStreamReader InputRead= new InputStreamReader(fileIn);
+
+            char[] inputBuffer= new char[READ_BLOCK_SIZE];
+            String s="";
+            int charRead;
+
+            while ((charRead=InputRead.read(inputBuffer))>0) {
+
+                String readstring=String.copyValueOf(inputBuffer,0,charRead);
+                s +=readstring;
+            }
+            InputRead.close();
+            textmsg.setText(s);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
